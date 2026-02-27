74f99dcdcc7cb348795d091a65fc41c0952fc942
Saurabh Vishwakarma <saurabh.vishw.007@gmail.com>
2026-02-27 10:34:52 +0530

    AI

--- src/main.py
+++ src/main.py
@@ -1,7 +1,62 @@
+import os
+import json
+from google.genai import types
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from google import genai
+from pydantic import BaseModel
 
 app=FastAPI()
-@app.get("/")
-def home():
-    return {"message": "Hellow World"}
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("API_Key"))
+
+class UserRequest(BaseModel):
+    prompt: str
+
+
+
+@app.post("/execute")
+def respond(request: UserRequest):
+    system_rules = """
+    You are an intelligent desktop assistant and your name is KEMO. 
+    
+    You have two main capabilities:
+    1. Execute OS tasks (opening/closing apps etc).
+    2. Answer general questions and chat naturally with the user.
+    
+    Available OS actions:
+    1. 'openApp' (Requires argument: 'app_name')
+    2. 'closeApp' (Requires argument: 'app_name')
+
+    RULES:
+    - If the user asks you to do an OS task, populate the 'tasks' array.
+    - If the user asks a general question (like the coding help, or general chat), leave the 'tasks' array EMPTY and put your answer in the 'message' field.
+    
+    You MUST respond strictly in the following JSON format. Do not include markdown formatting.
+    {{
+        "tasks": [
+            {{
+                "action": "open_app",
+                "arguments": {{
+                    "app_name": "whatsapp"
+                }}
+            }}
+        ],
+        "message": "Your conversational response or answer to the user's question goes here."
+    }}
+    """
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview", 
+        contents=request.prompt,
+        config=types.GenerateContentConfig(
+            system_instruction=system_rules,
+            response_mime_type="application/json",
+        ),
+    )
+    aiResponse = json.loads(response.text)
+    tasks = aiResponse.get("tasks", [])
+    
+
+    return {"message": aiResponse.get("message", "")}
 
